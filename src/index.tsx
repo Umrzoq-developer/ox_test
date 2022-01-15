b7ed523acaf65637e26ddf1a5a2c3dc0635783f6
umrzoq_toshkentov <utoshkentov@gmail.com>
2022-01-15 20:32:33 +0500

    fix: application

--- src/index.tsx
+++ src/index.tsx
@@ -28,17 +28,17 @@ ReactDom.render(
     document.getElementById('root'),
 );
 
-serviceWorker.register();
+// serviceWorker.register();
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('/service-worker.js')
-            .then((registration) => {
-                console.log('SW registered: ', registration);
-            })
-            .catch((registrationError) => {
-                console.log('SW registration failed: ', registrationError);
-            });
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//         navigator.serviceWorker
+//             .register('/service-worker.js')
+//             .then((registration) => {
+//                 console.log('SW registered: ', registration);
+//             })
+//             .catch((registrationError) => {
+//                 console.log('SW registration failed: ', registrationError);
+//             });
+//     });
+// }
